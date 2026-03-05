14e3b52175726e662510eb1aaf0c392fea1b79fb
Zaeema Nashath <96104978+zaeema-n@users.noreply.github.com>
2026-03-05 09:52:32 +0530

    Add missing csvs, fix folder names and transaction ids (#27)
    * Move secretary folder
    
    * Add missing files
    
    * Fix folder names and transaction ids

--- load_ak_sec_data.sh
+++ load_ak_sec_data.sh
@@ -9,11 +9,12 @@ BASE="$(pwd)/data/secretaries/Anura Kumara Dissanayake"
 
 ./orgchart -data "$BASE/2024-10-03/2405-14"  -type secretary
 
-./orgchart -data "$BASE/2024-11-19/2413-25-1"  -type secretary
-./orgchart -data "$BASE/2024-11-20/2413-25-2"  -type secretary
-./orgchart -data "$BASE/2024-11-21/2413-25-3"  -type secretary
-./orgchart -data "$BASE/2024-11-25/2413-25-4"  -type secretary
-./orgchart -data "$BASE/2024-11-28/2413-25-5"  -type secretary
+./orgchart -data "$BASE/2024-11-18/2413-25-1"  -type secretary
+./orgchart -data "$BASE/2024-11-19/2413-25-2"  -type secretary
+./orgchart -data "$BASE/2024-11-20/2413-25-3"  -type secretary
+./orgchart -data "$BASE/2024-11-21/2413-25-4"  -type secretary
+./orgchart -data "$BASE/2024-11-25/2413-25-5"  -type secretary
+./orgchart -data "$BASE/2024-11-28/2413-25-6"  -type secretary
 
 ./orgchart -data "$BASE/2024-12-03/2413-52-1"  -type secretary
 ./orgchart -data "$BASE/2024-12-11/2413-52-2"  -type secretary
@@ -25,5 +26,6 @@ BASE="$(pwd)/data/secretaries/Anura Kumara Dissanayake"
 ./orgchart -data "$BASE/2025-06-23/2444-09"  -type secretary
 
 ./orgchart -data "$BASE/2025-10-10/2460-27-1"  -type secretary
-./orgchart -data "$BASE/2025-10-14/2460-27-2"  -type secretary
-./orgchart -data "$BASE/2025-10-16/2460-27-3"  -type secretary
+./orgchart -data "$BASE/2025-10-11/2460-27-2"  -type secretary
+./orgchart -data "$BASE/2025-10-14/2460-27-3"  -type secretary
+./orgchart -data "$BASE/2025-10-16/2460-27-4"  -type secretary
