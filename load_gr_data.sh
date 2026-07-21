6541b5e16fd0edfe1291c99a03c7d7fceac5b984
ChanukaUOJ <chamithroxmpc@gmail.com>
2026-07-21 20:58:54 +0530

    2158-18 gazette

--- load_gr_data.sh
+++ load_gr_data.sh
@@ -30,6 +30,7 @@
 ./orgchart -data "$(pwd)/data/orgchart/Gotabaya Rajapaksa/2020-01-08/"
 ./orgchart -data "$(pwd)/data/orgchart/Gotabaya Rajapaksa/2020-01-09/"
 ./orgchart -data "$(pwd)/data/orgchart/Gotabaya Rajapaksa/2020-01-13/"
+./orgchart -data "$(pwd)/data/orgchart/Gotabaya Rajapaksa/2020-01-14/"
 
 ./orgchart -data "$(pwd)/data/orgchart/Gotabaya Rajapaksa/2020-01-22/2159_15/"
 ./orgchart -data "$(pwd)/data/orgchart/Gotabaya Rajapaksa/2020-01-22/2159_21/"
